ac10eef6f3ec52c320db98a07e8eea5729db6a55
TB <tirth.bhadeshiya@marwadieducation.edu.in>
2026-06-01 12:46:37 +0530

    Add files via upload

---
+++ php2.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Variable</title>
+    </head>
+    <body>
+        <?php
+            $txt ="INDIA";
+            echo "<h1>I love $txt!</h1>";
+            echo "<h2>PHP is Fun!</h2>";
+        ?>
+    </body>
+</html>
